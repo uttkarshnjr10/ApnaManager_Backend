901b263c5a77132176776bbc1b37b4843653aad2
uttkarshnjr10 <uttkarshkumar687@gmail.com>
2025-12-28 18:13:09 +0530

    fix the auth feature issue in production

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -16,7 +16,7 @@ const generateToken = (id, role, username) => {
 const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
 };
 
